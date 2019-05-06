8f913bdc3ec649430c9ed35d0073a3aba0e87648
Alex Shekunsky <shekunsky@gmail.com>
2019-05-06 23:58:27 +0300

    Initial Commit

---
+++ WindmillTestTask/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  WindmillTestTask
+//
+//  Created by Alex2 on 5/6/19.
+//  Copyright © 2019 Alex Shekunsky. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
